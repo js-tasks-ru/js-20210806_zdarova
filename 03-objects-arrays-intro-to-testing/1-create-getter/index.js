d6cde9af02965c758096f4b938841c816ef4b310
Maksim Mirbabaev <mmirbabaev@yotateam.com>
2021-08-19 10:26:04 +0300

    homework 2 done

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function (obj) {
+    const pathProps = path.split('.');
+    let currentObj = obj;
+    for (let prop of pathProps) {
+      if (!(prop in currentObj)) {
+        return undefined;
+      }
+      currentObj = currentObj[prop];
+    }
+    return currentObj;
+  };
 }
